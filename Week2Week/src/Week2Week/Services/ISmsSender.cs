583b5d7e5829f464257331d555092d1dec9fd31c
jammy <Jammylaird@yahoo.com>
2016-12-12 09:52:28 -0600

    commiting boilerplate code

---
+++ Week2Week/src/Week2Week/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week2Week.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
